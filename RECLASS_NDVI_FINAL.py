906c29b9e2f4f87fd5fa34c2a56836686e78dc61
LuanFaria <85500922+LuanFaria@users.noreply.github.com>
2025-05-23 09:07:35 -0300

    Update RECLASS_NDVI_FINAL.py

--- RECLASS_NDVI_FINAL.py
+++ RECLASS_NDVI_FINAL.py
@@ -7,11 +7,12 @@ import os
 from shapely.geometry import shape
 import pandas as pd
 import shutil
+from shapely.validation import make_valid
 
 #imputs
-raiz = 'C:/Users/luan.faria/Desktop/teste_5/'
-shp = 'BASE_TALHOES_NDVI_ARALCO_J1_2024.shp'
-ervas= ''
+raiz = "X:/Sigmagis/Projetos/GRUPO TEREOS/NDVI/2025/J2/MANDU"
+shp = 'BASE_TALHOES_NDVI_MANDU_J2_2025.shp'
+ervas= 'ERVAS_MANDU_J2_2025.shp'
 
 # PASTAS
 PASTA_SHAPES = os.path.join(raiz, 'Vetores/shape/', shp )
@@ -25,14 +26,30 @@ VETORIZADO='C:/CLASSIFICACAO/VETOR/'
 INTERSECT='C:/CLASSIFICACAO/INTERSECT/'#imputs
 MERGE='C:/CLASSIFICACAO/MERGE/'
 DESCOMPRIMIR='C:/CLASSIFICACAO/DESC/'
+MERGE_ERVAS='C:/CLASSIFICACAO/MERGE_ERVAS/'
+
 
 os.makedirs(os.path.join(SAIDA, 'REC'), exist_ok=True)
 os.makedirs(os.path.join(SAIDA, 'VETOR'), exist_ok=True)
 os.makedirs(os.path.join(SAIDA, 'INTERSECT'), exist_ok=True)
 os.makedirs(os.path.join(SAIDA, 'MERGE'), exist_ok=True)
+os.makedirs(os.path.join(SAIDA, 'MERGE_ERVAS'), exist_ok=True)
 os.makedirs(os.path.join(SAIDA, 'DESC'), exist_ok=True)
 
+
+#corrigir_geometria(shp)
+def corrigir_geometria(geometry):
+    if not geometry.is_valid:  # Verifica se a geometria é inválida
+        try:
+            # Tenta usar o make_valid se disponível
+            geometry = make_valid(geometry)
+        except Exception:
+            # Caso falhe, usa buffer de 0 como fallback
+            geometry = geometry.buffer(0)
+    return geometry
+
 def descomprimir_raster():
+    print('\nDescompactando img!\n')
     for filename in os.listdir(PASTA_RESAMPLE):
             if filename.endswith('.tif'):
                 file = os.path.join(PASTA_RESAMPLE, filename)
@@ -45,37 +62,42 @@ def descomprimir_raster():
                 with rasterio.open(output_file, "w", **profile) as dst:
                     dst.write(teste, 1)
 
+
 #reclassificação
 def processo():
-    descomprimir_raster()        
-    for filename in os.listdir(DESCOMPRIMIR):
+    descomprimir_raster()   
+    for filename in os.listdir(PASTA_RESAMPLE):
         if filename.endswith('.tif'):
-            file = os.path.join(DESCOMPRIMIR, filename)
+            file = os.path.join(PASTA_RESAMPLE, filename)
             output_file = os.path.join(RECLASS, filename)
 
             print('\nAbrindo o Raster: ',filename)
 
-            # falhas=float(input('Insira valores Falhas: '))
-            # MediaBaixa=float(input('Insira valores média baixa: '))
-            # Media=float(input('Insira valores média: '))
-            # MediaAlta=float(input('Insira valores média alta: '))
+            falhas=float(input('Insira valores Falhas: '))
+            MediaBaixa=float(input('Insira valores média baixa: '))
+            Media=float(input('Insira valores média: '))
+            MediaAlta=float(input('Insira valores média alta: '))
             # fundo = float(0)
-            
-            falhas=float(0.1)
-            MediaBaixa=float(0.2)
-            Media=float(0.3)
-            MediaAlta=float(0.4)
+
+            #Valores para teste
+            # falhas=float(0.1)
+            # MediaBaixa=float(0.2)
+            # Media=float(0.3)
+            # MediaAlta=float(0.4)  
             
             with rasterio.open(file) as src:
                 data = src.read(1)
+                
                 reclass_data =  np.where(data <= falhas, 1,
                                 np.where(data <= MediaBaixa, 2,
                                 np.where(data <= Media, 3,
                                 np.where(data <= MediaAlta, 4,
                                 np.where(data <= 1.0, 5, 6)))))
+
                 profile = src.profile
                 profile.update(dtype=rasterio.float32)
                 
+
                 with rasterio.open(output_file, "w", **profile) as dst:
                     dst.write(reclass_data, 1)
                 print('Reclassificação - OK')
@@ -134,14 +156,18 @@ def processo():
     print('\nAREA TOTAL SHAPE: ',total,'ha')
     dissolvido['BIOMASSA'] =  dissolvido['AREA_GIS'] / total * 100
 
-    #print(dissolvido['BIOMASSA'])
+    print(dissolvido['BIOMASSA'])
 
-    print('\nVALORES DE BIOMASSA EM % (DESCONSIDERANDO AS ERVAS):',
-        '\nBIOMASSA RUIM: ',dissolvido['BIOMASSA'][1]+dissolvido['BIOMASSA'][2],'%',
-        '\nBIOMASSA MÉDIA: ',dissolvido['BIOMASSA'][3],'%',
-        '\nBIOMASSA BOA: ',dissolvido['BIOMASSA'][4] + dissolvido['BIOMASSA'][5],'%')
-  
+    # print('\nVALORES DE BIOMASSA EM % (DESCONSIDERANDO AS ERVAS):',
+    #     '\nBIOMASSA RUIM: ',dissolvido['BIOMASSA'][1]+dissolvido['BIOMASSA'][2],'%',
+    #     '\nBIOMASSA MÉDIA: ',dissolvido['BIOMASSA'][3],'%',
+    #     '\nBIOMASSA BOA: ',dissolvido['BIOMASSA'][4] + dissolvido['BIOMASSA'][5],'%')
+    
+    dissolvido_salva = dissolvido.reset_index()
+    dissolvido_salva['GRIDCODE']=dissolvido_salva['GRIDCODE'].astype(int)
     dissolvido.to_file(saida_merge)
+    merge_sem_erva = os.path.join(MERGE_ERVAS,'merge_sem_erva.shp')
+    dissolvido_salva.to_file(merge_sem_erva)
 
 executa= True
 while executa:
@@ -150,26 +176,46 @@ while executa:
     if opcao == 'N':
         executa = False
         if ervas.endswith('.shp'):
+            print("\nPossui Ervas!")
             final = os.path.join(SAIDA,'INTERSECT'+shp[12:])
+            saida_erva_dis = os.path.join(MERGE_ERVAS,'ervas_dis.shp')
+            merge_com_erva = os.path.join(MERGE_ERVAS,'merge_final.shp')
+            saida_dif = os.path.join(MERGE_ERVAS,'saida_dif_erva.shp')
             shapfile = gpd.read_file(os.path.join(PASTA_SHAPES))
+            shapfile["geometry"] = shapfile["geometry"].apply(corrigir_geometria)
             shapfile['AREA_GIS']=shapfile.area/10000 #CRIANDO AREA GIS
 
             for di in os.listdir(MERGE):
                 if di.endswith('.shp'):
                     dissolv = gpd.read_file(os.path.join(MERGE,di))
                     erva = gpd.read_file(os.path.join(SHAPE_ERVAS))
+                    erva = erva.dropna(subset=["geometry"])  # Remove geometrias None antes de processar
+                    erva["geometry"] = erva["geometry"].apply(corrigir_geometria)
+                    dissolv["geometry"] = dissolv["geometry"].apply(corrigir_geometria)
         
-                    print('\nCRIANDO GRIDCODE 6 E ADICIONANDO AO MERGE DISSOLVIDO')
+                    print('\nDissolvendo ervas!\n')
                     ervas_dissolvidas = erva.dissolve() #dissolvendo ervas
-                    dif= dissolv.overlay(ervas_dissolvidas, how='difference') #dissolve
+                    ervas_dissolvidas.to_file(saida_erva_dis) #estou tentando fazer por parte para saber onde ta dando pau
+
+                    print("Diferença shape intersect com erva!\n")
+                    dif= dissolv.overlay(ervas_dissolvidas, how='difference') #dissolve 
+                    dif['GRIDCODE']=dif['GRIDCODE'].astype(int)
+                    dif.to_file(saida_dif)
+
+
+                    print("Merge entre ervas e shp!")
                     union = dif.overlay(ervas_dissolvidas, how='union')
                     union['GRIDCODE'][5]=6
                     dissolvido_final = union[['GRIDCODE','geometry']]
+                    #dissolvido_final['GRIDCODE']=dissolvido_final['GRIDCODE'].astype(int)
+                    dissolvido_final.to_file(merge_com_erva)
         
                     print('\nREALIZANDO INTERSECT ENTRE O SHAPE E O MERGE DISSOLVIDO!')
                     intersect_final= dissolvido_final.overlay(shapfile, how='intersection')
                     intersect_final['AREA_NDVI'] = intersect_final.area/10000
-                    intersect_final['GRIDCODE']=intersect_final['GRIDCODE'].astype(int)
+
+                    if not intersect_final['GRIDCODE'].apply(float.is_integer).all():
+                        intersect_final['GRIDCODE'] = intersect_final['GRIDCODE'].astype(int)
             intersect_final.to_file(final)
 
             #DELETAR PASTAS
@@ -178,11 +224,13 @@ while executa:
             shutil.rmtree(VETORIZADO)
             shutil.rmtree(INTERSECT)
             shutil.rmtree(MERGE)
+            shutil.rmtree(MERGE_ERVAS)
             shutil.rmtree(DESCOMPRIMIR)
             print('FINALIZADO!!')
         else:
             print('\nNão Possui Shape de Ervas!\n\nCRIANDO AREA GIS NO SHP')
             shapfile = gpd.read_file(os.path.join(PASTA_SHAPES))
+            shapfile["geometry"] = shapfile["geometry"].apply(corrigir_geometria)
             shapfile['AREA_GIS']=shapfile.area/10000 #CRIANDO AREA GIS
             final = os.path.join(SAIDA,'INTERSECT'+shp[12:])
             
@@ -191,6 +239,7 @@ while executa:
             for d in os.listdir(MERGE):
                 if d.endswith('.shp'):
                     dissolvido_ = gpd.read_file(os.path.join(MERGE,d))
+                    dissolvido_["geometry"] = dissolvido_["geometry"].apply(corrigir_geometria)
                     dissolvido_final = dissolvido_[['GRIDCODE','geometry']]
                     intersect_final= dissolvido_final.overlay(shapfile, how='intersection')
                     intersect_final['AREA_NDVI'] = intersect_final.area/10000
@@ -204,4 +253,6 @@ while executa:
             shutil.rmtree(INTERSECT)
             shutil.rmtree(MERGE)
             shutil.rmtree(DESCOMPRIMIR)
+            shutil.rmtree(MERGE_ERVAS)
             print('FINALIZADO!!')
+            
